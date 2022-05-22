a67eefe04da781af4201166273a9ba6bd9337497
Clark1945 <dmc950213@gmail.com>
2022-05-22 19:09:00 +0800

    修復yande.re爬圖的頁數Bug

--- Yande.re photo_catcher.py
+++ Yande.re photo_catcher.py
@@ -2,55 +2,54 @@ import requests
 from bs4 import BeautifulSoup
 import os
 
-Keyword=input('請輸入作品/畫家名稱(以羅馬拼音)：')
-url="https://yande.re/post?page=1&tags="+Keyword
+url=input('請輸入Url：')
+fileName=input('請輸入要儲存的資料夾名稱')
+frontUrl=url[:27]
+endUrl=url[28:]
 url2="https://yande.re"
 re=requests.get(url)
 soup=BeautifulSoup(re.text,'html.parser')
-soupFindPage=soup.find_all('div',{'class':'pagination'})
-if (soupFindPage == []):
-    NPage = 1
-else:
-    Page=""
-    for x in soupFindPage:
-        Page=x.text
-        NPage=int(Page[-8:-6].strip())
-# print(Page+"Hello")
-# print(Npage)
-for y in range(1,NPage+1):
-    url="https://yande.re/post?page="+str(y)+"&tags="+Keyword
-    url2="https://yande.re"
-    re=requests.get(url)
+surl=url.find("page=")
+eurl=url.find("&tags")
+nowPage=url[surl+5:eurl]
+#print(nowPage)當前頁
+totalPage=soup.find("div",{"role":"navigation"}).find_all("a")[-2].text
+#print(totalPage)總共頁
+
+now_dir = os.path.dirname(__file__)
+if not os.path.exists(fileName):
+    os.mkdir(fileName)
+os.chdir(now_dir+"\\" +fileName)
+
+for nPage in range(1,int(totalPage)+1):
+    re=requests.get(frontUrl+str(nPage)+endUrl)
     soup=BeautifulSoup(re.text,'html.parser')
     soupFind=soup.find_all('a',{'class':'thumb'}) 
+    #print(soupFind)
     pic=[]
-    pic_count = 0;
+    pic_count = 0
     for x in soupFind:
         string=""
         tem=x.get('href')
         string=url2+tem
         pic.append(string)
-        now_dir = os.path.dirname(__file__)
+        #每個圖片網址的URL
     for y in pic:
         re2=requests.get(y)
         soup2=BeautifulSoup(re2.text,'html.parser')
         soupFind2=soup2.find_all('img',id='image')
         pic2=[]
         for gg in soupFind2:
-            temp=gg.get('src')
-            pic2.append(temp)
+           temp=gg.get('src')
+           pic2.append(temp)
+        #print(pic2)
         for gh in pic2: 
             K=gh.find('/yande.re')
             gh2=gh[72:]
             r=requests.get(gh)
-            if not os.path.exists(now_dir+"\\"+Keyword):
-                os.mkdir("./" +Keyword)
-                os.chdir(now_dir+"\\" +Keyword)
             with open(gh2,'wb') as f:   
                 f.write(r.content)
                 pic_count +=1
-                print("擷取第%d頁，第%d張中......" %(NPage,pic_count))
+                print("擷取第%s頁，第%d張中......" %(nPage,pic_count))
                 
 print("擷取完成！")
-                
-                
